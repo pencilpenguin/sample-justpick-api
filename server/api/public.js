051e5a65eb989ca0cf0dc06168b8ab04f3563d91
pencilpenguin <irving9614@gmail.com>
2021-01-23 13:46:14 -0600

    add route get-listings-from-google-places

--- server/api/public.js
+++ server/api/public.js
@@ -34,4 +34,35 @@ router.get('/get-listings-from-yelp', async (req, res) => {
 
 });
 
+router.get('/get-listings-from-google-places', async (req, res) => {
+  let listings = {}
+  
+  const parameters = {
+    key: process.env.GCP_API_KEY,
+    input: req.query.input,
+    inputtype: 'textquery',
+    fields:'business_status,formatted_address,geometry,icon,name,photos,place_id,plus_code,types',
+    locationbias: `circle:${req.query.rad}@${req.query.lat},${req.query.long}`
+  }
+
+  const options = {
+    host: process.env.GOOGLE_MAPS_URL,
+    path: `/maps/api/place/findplacefromtext/json?${querystring.stringify(parameters)}`,
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+    },
+  };
+
+  try {
+    listings = await request(options);
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.json(listings);
+
+});
+
+
 module.exports = router;
