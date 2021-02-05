ab01270ab5b84f53b880b77bb59568ff2ed410d2
pencilpenguin <irving9614@gmail.com>
2021-02-05 16:59:41 -0600

    update circleci config

---
+++ server/services/services.js
@@ -0,0 +1,69 @@
+
+const querystring = require('querystring');
+
+const { request, httpRequest } = require('../utils/requests');
+
+
+
+/**
+ * Make a request to the Yelp Fusion API to get a list of
+ * restaurants near the latitude and longtitude provided.
+ */
+getListingsFromYelp = async (latitude, longitude) => {
+    let listings = {};
+
+    const parameters = {
+        latitude: latitude,
+        longitude: longitude
+    };
+
+    const options = {
+      host: process.env.YELP_URL,
+      path: `/v3/businesses/search?${querystring.stringify(parameters)}`,
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${process.env.YELP_API_KEY}`
+      },
+    };
+    
+    try {
+       listings = await request(options);
+       return listings
+    } catch (err) {
+      throw err
+    }
+}
+module.exports.getListingsFromYelp = getListingsFromYelp;
+
+/**
+ * 
+ */
+getLocationCoordinates = async (location) => {
+    //http://api.positionstack.com/v1/forward?access_key=f92285d7f16b5f8920d6953f00e874f4&query=mcallen
+
+    const parameters = {
+        access_key: process.env.POSITIONSTACK_ACCESSKEY,
+        query: location,
+    }
+
+    const options = {
+        host: process.env.POSITIONSTACK_URL,
+        path: `/v1/forward?${querystring.stringify(parameters)}`,
+        method: 'GET',
+    };
+
+    try {
+        resp = await httpRequest(options);
+        if (resp.data.length != 0) {
+            return resp["data"][0]
+        }
+        else {
+            throw new Error("Invalid location")
+        }
+    } catch (err) {
+        console.log(err)
+        throw new Error(err)
+    }
+}
+module.exports.getLocationCoordinates = getLocationCoordinates;
--- server/utils/requests.js
+++ server/utils/requests.js
@@ -1,5 +1,5 @@
 const https = require('https');
-
+const http = require('http');
 
 request = (options) => {
  return new Promise((resolve, reject) => {
@@ -18,7 +18,7 @@ request = (options) => {
 
     res.on('end', () => {
       console.log('Response complete');
-      console.log(JSON.parse(data));
+      // console.log(JSON.parse(data));
       resolve(JSON.parse(data));
     });
   });
@@ -32,4 +32,37 @@ request = (options) => {
 
  });
 }
-module.exports = request;
+module.exports.request = request;
+
+httpRequest = (options) => {
+  return new Promise((resolve, reject) => {
+   let request = http.request(options, (res) => {
+     if (res.statusCode !== 200) {
+       console.error(`Request unsuccessful. Code: ${res.statusCode}`);
+       res.resume();
+       reject(res);
+     }
+ 
+     let data = '';
+ 
+     res.on('data', (chunk) => {
+       data += chunk;
+     });
+ 
+     res.on('end', () => {
+       console.log('Response complete');
+       console.log(JSON.parse(data));
+       resolve(JSON.parse(data));
+     });
+   });
+ 
+   request.end();
+ 
+   request.on('error', (err) => {
+     console.error(`Encountered an error trying to make a request: ${err.message}`);
+     reject(err);
+   });
+ 
+  });
+ }
+module.exports.httpRequest = httpRequest;
